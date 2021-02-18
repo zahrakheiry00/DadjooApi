ed63b237dabef1a020f3470bb88c51c6b5386fe9
zahrakheiry00 <zahra.kheiryashkuh00@gmail.com>
2021-02-18 18:03:29 +0330

    Add chat.

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
 from flask_cors import CORS
-app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 import psycopg2
 import json
+
+ROOM = 'room'
+from flask_socketio import SocketIO,join_room, leave_room
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 class Database:
  def __init__(self):
@@ -12,7 +18,7 @@ class Database:
  def connect(self):
   if self.conn is None:
    try:
-    self.conn = psycopg2.connect(database="????", user="postgres", password="????",host="localhost", port="5432")
+    self.conn = psycopg2.connect(database="dadjoo8", user="postgres", password="Navid16110202*",host="localhost", port="5432")
    except psycopg2.DatabaseError as e:
     print(e)
 
@@ -51,8 +57,167 @@ def login():
     return(y)
 
 
+@app.route('/api/dadjoo_accept_expert', methods=['POST'])
+def dadjoo_accept_expert():
+    param=request.json
+    expertid=param['expertid'];
+    userid=param['userid'];
+    str=f'SELECT public.dadjoo_accept_expert(\'{expertid}\',\'{userid}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
 
-app.run()
+
+@app.route('/api/dadjoo_add_freetime', methods=['GET ','POST'])
+def dadjoo_add_freetime():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_add_freetime(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_delete_freetime', methods=['GET ','POST'])
+def dadjoo_delete_freetime():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_delete_freetime(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_get_list_of_freetime', methods=['GET','POST'])
+def dadjoo_get_list_of_freetime():
+    param=request.json
+    d = json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_get_list_of_freetime(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_insert_file', methods=['GET','POST'])
+def dadjoo_insert_file():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_insert_file(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_insert_listfile', methods=['GET','POST'])
+def dadjoo_insert_listfile():
+    param=request.json
+    d = json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_insert_listfile(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_login', methods=['POST'])
+def dadjoo_login():
+    param=request.json
+    tusername=param['tusername'];
+    tpass=param['tpass'];
+    str=f'SELECT public.dadjoo_login(\'{tusername}\',\'{tpass}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_reg_client', methods=['GET ','POST'])
+def dadjoo_reg_client():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_reg_client(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_reg_client1', methods=['GET','POST'])
+def dadjoo_reg_client1():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_reg_client1(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_send_message', methods=['GET','POST'])
+def dadjoo_send_message():
+    param=request.json
+    d = json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_send_message(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+# @app.route('/api/dadjoo_specialty_list', methods=['GET','POST'])
+# def dadjoo_get_client_info():
+#     str=f'SELECT public.dadjoo_specialty_list(\'\');';
+#     db = Database()
+#     data = db.update_rows(str)
+#     y = json.dumps(data)
+#     return(y)
+
+
+@app.route('/api/dadjoo_update_freetime', methods=['GET ','POST'])
+def dadjoo_update_freetime():
+    param=request.json
+    d=json.dumps(param['intext'])
+    str=f'SELECT public.adjoo_update_freetime(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@app.route('/api/dadjoo_get_client_info', methods=['GET','POST'])
+def dadjoo_get_client_info():
+    param=request.json
+    d = json.dumps(param['intext'])
+    str=f'SELECT public.dadjoo_get_client_info(\'{d}\');';
+    db = Database()
+    data = db.update_rows(str)
+    y = json.dumps(data)
+    return(y)
+
+
+@socketio.on('connect')
+def connect():
+    socketio.emit('ready', room=ROOM, skip_sid=request.sid)
+    join_room(ROOM)
+
+
+@socketio.on('disconnect')
+def disconnect():
+    leave_room(ROOM)
+
+
+@socketio.on('data')
+def data(data):
+    if data :
+      socketio.emit('data', data, room=ROOM,skip_sid=request.sid)
+
+
+socketio.run(app)
 print ("Operation done successfully");
 conn.close()
 
